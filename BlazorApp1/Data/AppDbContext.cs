fe32a07178c2df465b2adea93222fe95768be470
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-08-24 12:06:36 -0300

    First commit

---
+++ BlazorApp1/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite(@"Data Source=Database\test.db");
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BlazorApp1/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp1.Data;
+
+namespace BlazorApp1.Services
+{
+    public class UserService
+    {
+        public async Task AddNewUser(User user)
+        {
+            var context = new AppDbContext();
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<User>> FetchUsers()
+        {
+            var context = new AppDbContext();
+            List<User> users = context.Users.ToList();
+            return users;
+        }
+    }
+}
